7f974f2e2f0dacbf63703a39f86d3cb1ede69fe6
Stageddat <hello@stageddat.dev>
2026-06-25 21:24:33 +0200

    feat: update server port and add user/entries routes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -11,6 +12,7 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "shelter node is running :)")
 	})
@@ -19,5 +21,41 @@ func main() {
 		w.Write([]byte("ok"))
 	})
 
-	http.ListenAndServe(":3000", r)
+	r.Route("/v1", func(r chi.Router) {
+		r.Route("/user", func(r chi.Router) {
+			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusNotImplemented)
+				json.NewEncoder(w).Encode(struct {
+					Status string `json:"status"`
+				}{
+					Status: "not implemented",
+				})
+			})
+			r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("ok"))
+			})
+		})
+
+		r.Route("/entries", func(r chi.Router) {
+			r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("ok"))
+			})
+			r.Post("/", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("ok"))
+			})
+			r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("ok"))
+			})
+			r.Put("/{id}", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("ok"))
+			})
+			r.Delete("/{id}", func(w http.ResponseWriter, r *http.Request) {
+				w.Write([]byte("ok"))
+			})
+		})
+	})
+
+	fmt.Println("server running on port 4123 :)")
+	http.ListenAndServe(":4123", r)
 }
